885bcf0bd9b08100022bb09281c729247661e9de
Palak Agarwal <agarwalpalak081@gmail.com>
2022-10-08 15:39:05 +0530

    new file

---
+++ dayy/day_14.py
@@ -0,0 +1,17 @@
+# GUI - Graphicla User Interface
+
+# Libraries
+###################
+# 1. Tkinter
+# 2. PyQT
+# 3. Turtle
+
+import tkinter as ttk
+app = ttk.Tk()
+app.title('My App')
+app.geometry('600x400')
+
+ttk.Label(app, text = 'A simple Text Label').place(x=50,y=50)
+ttk.Label(app, text = 'Heyy Palak here').place(x=70,y=70)
+
+app.mainloop()
