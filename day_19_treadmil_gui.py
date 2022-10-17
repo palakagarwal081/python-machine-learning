f2c244db5d12d0cbcbed3be790698ca4eada3a9a
Palak Agarwal <agarwalpalak081@gmail.com>
2022-10-17 20:39:22 +0530

    treadmil gui

--- day_19_treadmil_gui.py
+++ day_19_treadmil_gui.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import tkinter as ttk
+from PIL import Image,ImageTk
 
 data = pd.read_csv('treadmil-users.csv')
 
@@ -11,8 +12,12 @@ app.geometry('600x300')
 
 ## Radio button
 graphs = ttk.Variable(app)
-values = {'Pair Plot': 'sns.pairplot(data = data)','Joint Plot': "sns.jointplot(data = data, x='{col1}',y = '{col2}')",
-            'Bar Plot':"sns.barplot(data = data, x= '{col1}', y = '{col2}')"}
+values = {
+    'Pair Plot': 'sns.pairplot(data = data)',
+    'Joint Plot': "sns.jointplot(data = data, x='{col1}',y = '{col2}')",
+    'Bar Plot':"sns.barplot(data = data, x= '{col1}', y = '{col2}')",
+    'Box Plot': "sns.boxplot(data = data,x = '{col1}', y = '{col2}')"
+}
 
 graphs.set(values['Pair Plot'])
 y = 10
@@ -39,9 +44,45 @@ col3.set(values[0])
 ttk.Label(app,text = 'Column 3').place(x = 150 ,y = 150)
 ttk.OptionMenu(app, col3, *values).place(x = 150, y = 180) 
 
+# Canvas
+cnv = ttk.Canvas(app, width=250, height=200)
+cnv.place(x = 300, y = 60)
+
+# Label
+result = ttk.Variable(app)
+ttk.Label(app,textvariable = result).place(x=300,y=300)
+
 def show():
-   # print(graphs.get())
-   print(col1.get())
+    global img
+    global cnv
+
+    column1 = col1.get()
+    column2 = col2.get()
+    column3 = col3.get()
+    g = graphs.get()
+    if 'col1' in g:
+        if column1 == 'Select':
+            result.set('column 1 must be selected')
+            return
+    if 'col2' in g:
+        if column2 == 'Select':
+            result.set('column 2 must be selected')
+            return
+    if 'col3' in g:
+        if column3 == 'Select':
+            result.set('column 3 must be selected')
+            return
+    fig = plt.figure(figsize = (5,2))
+    eval(g.format(col1 = column1, col2 = column2, col3 = column3))
+    fig.savefig('graph.png')
+    img = ImageTk.PhotoImage(
+        Image.open('graph.png').resize((250,200))
+    )
+    cnv.create_image(0,0,anchor = ttk.NW , image = img)   # NW = north west second quadrant anchor means origin
+    # plt.show()
+    # print(graphs.get())
+    # print(col1.get(),col2.get(),col3.get())
+   
 
 ttk.Button(app,text = 'Show',command = show).place(x=400,y=10)
 
